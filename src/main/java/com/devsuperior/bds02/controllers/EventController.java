11f42b840beb4040c3e45e863fbf0352b7f1934f
Bruno Guimarães <brunoelinton@outlook.com>
2022-07-01 10:09:02 -0300

    Integration Tests from Project: Event City - Concluded

--- src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -1,23 +1,14 @@
 package com.devsuperior.bds02.controllers;
 
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.dto.EventDTO;
-import com.devsuperior.bds02.services.CityService;
 import com.devsuperior.bds02.services.EventService;
 
 @RestController
